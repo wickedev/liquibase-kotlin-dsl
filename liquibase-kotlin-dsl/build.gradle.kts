d6d6019d3d4e5a9375cc04d9cd09d8482e371d93
wickedev <orange881217@gmail.com>
2020-10-26 16:27:23 +0900

    script-definition-3.0.0 publish

--- liquibase-kotlin-dsl/build.gradle.kts
+++ liquibase-kotlin-dsl/build.gradle.kts
@@ -1,8 +1,25 @@
 plugins {
-    publish
+    id("maven-publish")
+    id("org.jetbrains.kotlin.jvm")
 }
 
 dependencies {
     api(project(":script-definition"))
     implementation("org.liquibase:liquibase-core:3.10.2")
+}
+
+publishing {
+    publications {
+
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        repositories {
+            maven {
+                url = uri("$rootDir/maven-repo")
+            }
+        }
+    }
 }
--- script-definition/build.gradle.kts
+++ script-definition/build.gradle.kts
@@ -1,7 +1,24 @@
 plugins {
-    publish
+    id("maven-publish")
+    id("org.jetbrains.kotlin.jvm")
 }
 
 dependencies {
     implementation(kotlin("scripting-jvm"))
+}
+
+publishing {
+    publications {
+
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        repositories {
+            maven {
+                url = uri("$rootDir/maven-repo")
+            }
+        }
+    }
 }
